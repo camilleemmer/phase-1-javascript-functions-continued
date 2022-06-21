b9ed663fe353eab1b1d5732c76a6585a1ab130a5
Camille Emmer <camilleemmer@gmail.com>
2022-06-21 16:06:58 -0600

    commit tree aa0033f82a393e8229bc02c5a8912cc3c1b6598d

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(thingToDo = "roller-skate") {
+    return(`This Saturday, I want to ${thingToDo}!`);
+   
+}
+
+function mondayWork(activity = "go to the office") {
+    return (`This Monday, I will ${activity}.`);
+}
+ mondayWork( "work from home.");
+
+
+
+  function wrapAdjective(part1 = "**") {
+    return function (message = "special") {
+      return (`You are ${part1}${message}${part1}!`);
+    };
+  }
+
+wrapAdjective("%")(" a dedicated programmer"); 
